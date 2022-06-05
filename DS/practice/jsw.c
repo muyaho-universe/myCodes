4e87070a385987badd28643d26aa76443b86e0fd
muyaho-universe <kimdaesk@gmail.com>
2022-06-05 22:30:50 +0900

    Complete jsw.c

--- DS/practice/jsw.c
+++ DS/practice/jsw.c
@@ -3,43 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MAX 100
-int front=-1;
-int rear=-1;
-char queue[MAX][20];
 int count = 0;
 
-int IsEmpty(){
-    if(front==rear)//front와 rear가 같으면 큐는 비어있는 상태 
-        return 1;
-    else return 0;
-}
-int IsFull(){
-    int tmp=(rear+1) % MAX; //원형 큐에서 rear+1을 MAX로 나눈 나머지값이
-    if(tmp==front)//front와 같으면 큐는 가득차 있는 상태 
-        return 1;
-    else
-        return 0;
-}
-void addq( char value[20]){
-    if(IsFull())
-        printf("Queue is Full.\n");
-    else{
-        rear = (rear+1)%MAX;
-        strcpy(queue[rear], value);
-    }
- 
-}
+#define ESC 0x1b //ESC의 아스키 코드 값을 정의
+#define MAX_QUEUE_SIZE 100
+#define MAX_STRING_SIZE 256
+#define MAX 100
 
-const char* deleteq(){
-    if(IsEmpty())
-        printf("Queue is Empty.\n");
-    else{
-        front = (front+1)%MAX;
-        return queue[front];
-    }
-}
+typedef struct
+{
+  char body[MAX_QUEUE_SIZE][MAX_STRING_SIZE];
+  int front;
+  int rear;
+  int length;
+} QUEUE ;
+
+QUEUE* Q_Create();
+void Q_Free(QUEUE *queue);
+void Q_Insert(QUEUE *queue, char value[20]);
+char* Q_Delete(QUEUE *queue);
+int Q_Length(QUEUE *queue);
+void Q_PrintAll(QUEUE *queue);
+void usage();
+int Q_IsFull(QUEUE *queue);
+int Q_IsEmpty(QUEUE *queue);
 
 struct node {
     char item[20];
@@ -47,13 +34,14 @@ struct node {
     struct node* right;
 };
 
+
 // Inorder traversal
-void inorderTraversal(struct node* root) {
+void inorderTraversal(struct node* root, QUEUE* q) {
     if (root == NULL) return;
-    inorderTraversal(root->left);
+    inorderTraversal(root->left, q);
     printf("%s ->", root->item);
-    addq(root->item);
-    inorderTraversal(root->right);
+    Q_Insert(q,root->item);
+    inorderTraversal(root->right, q);
 }
 
 // preorderTraversal traversal
@@ -120,7 +108,6 @@ void insert_node(char t[20], struct node* root) {
     {
         if (strcmp(root->item,t) ==0)
         {
-            printf("Insertion Failed : the Key %d already exists.\n" , t) ;
             return;
         }
         
@@ -184,8 +171,8 @@ struct node * delete_node(struct node * root, char value[20]){
 
 int main() {
     struct node* root = createNode("korea");
-    
-    int q2arr[MAX];
+    QUEUE* queue =  Q_Create();
+    char q2arr[MAX][20];
 
     insertLeft(root, "data");
     insertRight(root, "Ahn");
@@ -205,7 +192,7 @@ int main() {
     insertRight(root->right, "university");
 
     printf("Inorder traversal \n");
-    inorderTraversal(root);
+    inorderTraversal(root, queue);
 
     printf("\nPreorder traversal \n");
     preorderTraversal(root);
@@ -214,7 +201,7 @@ int main() {
     postorderTraversal(root);
 
     for (int i = 0; i < count; i++){
-        q2arr[i] = deleteq();
+        strcpy(q2arr[i], Q_Delete(queue));
     }
 
     struct node* root2 = createNode2(q2arr[0]);
@@ -224,7 +211,7 @@ int main() {
 
     printf("\n-----------------BST-----------------\n");
     printf("Inorder traversal \n");
-    inorderTraversal(root2);
+    inorderTraversal(root2, queue);
 
     printf("\nPreorder traversal \n");
     preorderTraversal(root2);
@@ -234,3 +221,136 @@ int main() {
     
 }
 
+QUEUE *Q_Create()
+{
+    /*
+    생성된 QUEUE 구조체를 초기화 (front=0, rear=0, length=0)
+    첫번째 인자 : QUEUE 구조체 포인터 변수
+    리턴 값 : 큐 구조체 포인터
+    */
+
+    QUEUE *queue = (QUEUE *) malloc (sizeof(QUEUE)); // 동적 메모리 할당
+
+    // 구조체 초기화
+    queue->front = 0;
+    queue->rear = 0;
+    queue->length = 0;
+
+    return queue;  // 주소 반환
+}
+
+void Q_Free(QUEUE *queue)
+{
+    /*  메모리에 동적으로 생성된 QUEUE 구조체를 삭제
+    첫번째 인자 : QUEUE 구조체 포인터 변수 */
+    
+    /*프로그램의 내용이 배열인 관계로
+    큐 구조체 생성시에만 동적 메모리 할당을 했으므로
+    그 부분만 해제 한다.*/
+    
+    free(queue);
+}
+void Q_Insert(QUEUE *queue, char value[20])
+{
+    /*
+    큐의 Body에 아이템을 추가하는 함수.(Length 증가)
+    Q_IsFull() 함수를 통해 먼저 큐의 모든 공간이 꽉 차있는지 검사
+    gets()  함수를 통해 사용자로부터 문자열을 입력 받아 큐에 저장한다.
+
+    큐의 rear를 1증가하여 최초 배열의 1번 인덱스 위치에 값을 저장한다.
+    q->rear = (q->rear+1) % MAX_QUEUE_SIZE
+    %연산을 통해 큐의 index값을 0~MAX_QUEUE_SIZE-1 범위 내로 한정한다.
+    이로 인해 rear가 MAX_QUEUE_SIZE-1, 즉 배열의 마지막 인덱스까지 값을 채웠을 경우
+    아직 큐의 size가 MAX_QUEUE_SIZE보다 작다면 그 다음 다시 0번 index로 값을 채우기 시작한다.
+
+    첫 번째 인자 : QUEUE 구조체 포인터 변수
+    */
+    
+    if(Q_IsFull(queue) == 0)  //꽉차 있지 않으면 즉, 조금이라도 비어 있으면
+    {
+       
+
+        (queue->rear) = (queue->rear+1) % MAX_QUEUE_SIZE;
+        //인덱스 값을 증가 시킨다.
+        strcpy(queue->body[queue->rear],value);
+        (queue->length)++;
+        //데이터가 하나 증가 했으므로 길이값을 1올린다.
+        
+    }
+
+    else // 꽉 차있으면.
+    {printf("Circular_Queue space is FULL !!! \n");}
+ 
+}
+
+char* Q_Delete(QUEUE *queue)
+{ /*
+ 큐의 front 인덱스가 가르키는 아이템(문자열)을 반환하는 함수.(Length 감소)
+ Q_IsEmpty() 함수를 통해 먼저 큐의 모든 공간이 비어 있는지 검사
+ front 인덱스 값을 증가시켜 최초 큐의 1번째 인덱스의 값을 참조 할 수 있도록 한다.
+ q->front = (q->front+1) % MAX_QUEUE_SIZE;
+
+ 그 다음 strcpy() 함수를 통해 아이템(문자열)을 복사한다.
+ char * strcpy(char *, const char *);  - string.h 해더추가
+ 앞 문자열의 처음에 뒤 문자열을 복사하여 그 복사된 문자열을 반환한다.
+ 앞 문자열은 수정되지만 뒤 문자열은 수정될 수 없다.
+ 첫 번째 인자 : QUEUE 구조체 포인터 변수
+ 리턴 값 : 큐의 front가 가르키고 있던 아이템(문자열)
+ */
+    char save_string[20];
+    if(Q_IsEmpty(queue) == 0) // 조금이라도 차 있으면
+    {
+        (queue->front) = (queue->front+1) % MAX_QUEUE_SIZE;
+        //front의 인덱스 값을 변경 시킨다.
+        
+        strcpy( save_string,  queue->body[queue->front] );
+        // 어떤 값이 제거가 되었는지 출력하기 위해 복사. 후에 리턴 값
+        
+        (queue->length)--; //데이터가 제거가 되었으므로 길이 값과 포인터 값을 변경합니다.
+        
+        return save_string;
+    }
+}
+
+int Q_Length(QUEUE *queue)
+{ 
+    return queue->length;
+}
+
+void Q_PrintAll(QUEUE *queue)
+{ 
+    int counter = 0;
+
+    printf("<Front> ");
+    if(Q_IsEmpty(queue) == 0)
+    {
+        counter = (queue->front+1) % MAX_QUEUE_SIZE ;
+        /*인덱스 값을 맞추기 위해서 +1을 저정합니다.
+        실제 프로그램 시작후 front 에는 쓰레기 값이 저장되어 있습니다.*/
+
+        while( counter != (queue->rear + 1)%MAX_QUEUE_SIZE ) // front ~ rear 까지 출력
+        { 
+            printf(" \"%s\" ", queue->body[counter]);
+            counter = (counter +1) % MAX_QUEUE_SIZE;
+        }
+    
+    }
+    printf(" <Rear>\n");
+}
+
+
+int Q_IsFull(QUEUE *queue)
+{
+    if(queue->front == (queue->rear+1) % MAX_QUEUE_SIZE)
+    { return 1; }
+    else
+    { return 0; }
+}
+
+int Q_IsEmpty(QUEUE *queue)
+{
+    if(queue->front == queue->rear)
+    {return 1; }
+    else
+    {return 0; }
+}
