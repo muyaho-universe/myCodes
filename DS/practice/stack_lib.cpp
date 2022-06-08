aa5bf3acb1f7082bd6a9b17fa7187d4614da9e9e
muyaho-universe <kimdaesk@gmail.com>
2022-06-08 16:59:35 +0900

    Create stack_lib.cpp

---
+++ DS/practice/stack_lib.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    stack <int> s1;
+    int i;
+    s1.push(1);
+    s1.push(2);
+    i = s1.pop();
+    cout << i <<endl;
+}
