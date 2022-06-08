2b9b820fa316e6ec8fecab167ca043cfcc1bc344
muyaho-universe <kimdaesk@gmail.com>
2022-06-08 17:14:44 +0900

    Make some practice set

--- DS/practice/vector.cpp
+++ DS/practice/vector.cpp
@@ -6,17 +6,25 @@ using namespace std;
 
 int main()
 {
-    vector <int> a;
-    a.push_back(2);
-    a.push_back(4);
-    a.push_back(6);
-    cout << a.size() << endl;
-    cout << a[1] << endl;
+    vector <int> a(10);
+    vector <int> :: iterator p;
 
-    a.pop_back();
-    a.push_back(8);
-    cout << a.front() << endl;
-    cout << a.back() << endl;
+    for (int k = 0; k < 10; k ++)
+        a[k] = k;
+
+    for ( p = a.begin(); p < a.end(); p++)
+        cout << *p << endl;
+    
+    // a.push_back(2);
+    // a.push_back(4);
+    // a.push_back(6);
+    // cout << a.size() << endl;
+    // cout << a[1] << endl;
+
+    // a.pop_back();
+    // a.push_back(8);
+    // cout << a.front() << endl;
+    // cout << a.back() << endl;
 
     return 0;
 }
