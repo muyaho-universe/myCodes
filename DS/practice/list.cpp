37f714ca3892d3b7c85aef5b3a40c7026d6499b3
muyaho-universe <kimdaesk@gmail.com>
2022-06-10 20:18:27 +0900

    Do the initial commit

---
+++ DS/practice/list.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+
+int main()
+{
+    list <int> a;
+    list <int>::iterator p;
+
+    a.push_back(25);
+    a.push_back(17);
+    a.push_back(33);
+    a.push_back(19);
+
+    a.sort();
+    
+    for(p = a.begin(); p!=a.end(); p++)
+        cout << *p << " : ";
+    cout << endl;
+    return 0
+}
