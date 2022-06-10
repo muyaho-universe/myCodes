c9a5937ca3391a8c09f0eca5739b164d87ab1adb
muyaho-universe <kimdaesk@gmail.com>
2022-06-10 20:05:09 +0900

    Add testsets

--- DS/practice/stack_lib.cpp
+++ DS/practice/stack_lib.cpp
@@ -8,11 +8,22 @@ using namespace std;
 int main()
 {
     stack <int> s1;
-    int i;
-    s1.push(1);
-    s1.push(2);
-    i = s1.top();
-    s1.pop();
-    cout << i <<endl;
+    // int i;
+    // s1.push(1);
+    // s1.push(2);
+    // i = s1.top();
+    // s1.pop();
+    // cout << i <<endl;
+
+    queue <int> q1;
+    q1.push(1);
+    q1.push(2);
+    q1.push(3);
+    q1.push(4);
+    q1.pop();
+    cout << q1.front() << endl;
+    cout << q1.back() << endl;
+    cout << q1.size() << endl;
+    
     return 0;
 }
